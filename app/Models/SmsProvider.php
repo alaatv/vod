f3159ac6412c499acf9566e03f5547892eaa73b5
Arshia <arshiayazdian@gmail.com>
2023-10-19 00:55:55 +0330

    remove comment

--- app/Models/SmsProvider.php
+++ app/Models/SmsProvider.php
@@ -9,30 +9,6 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Arr;
 use Eloquent;
 
-/**
- * App\SmsProvider
- *
- * @property int $id
- * @property string $number شماره سامانه پیامکی
- * @property int $cost هزینه ارسال با شماره سامانه مورد نظر
- * @property int $enable فعال یا غیرفعال بودن شماره سامانه
- * @property string|null $description
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property string|null $deleted_at
- * @method static Builder|SmsProvider newModelQuery()
- * @method static Builder|SmsProvider newQuery()
- * @method static Builder|SmsProvider query()
- * @method static Builder|SmsProvider whereCost($value)
- * @method static Builder|SmsProvider whereCreatedAt($value)
- * @method static Builder|SmsProvider whereDeletedAt($value)
- * @method static Builder|SmsProvider whereDescription($value)
- * @method static Builder|SmsProvider whereEnable($value)
- * @method static Builder|SmsProvider whereId($value)
- * @method static Builder|SmsProvider whereNumber($value)
- * @method static Builder|SmsProvider whereUpdatedAt($value)
- * @mixin Eloquent
- */
 class SmsProvider extends Model
 {
     use Scopes;
