dc145691f168f7a0df5c228dff9afb5bd3b454f9
arshia <arshiayazdian@gmail.com>
2023-12-19 17:59:24 +0330

    comment web URL

--- app/Http/Resources/Url.php
+++ app/Http/Resources/Url.php
@@ -4,20 +4,18 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 
-
 class Url extends AlaaJsonResource
 {
     /**
      * Transform the resource into an array.
      *
      * @param  Request  $request
-     *
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'web' => $this->when(isset($this->url), $this->url),
+            //            'web' => $this->when(isset($this->url), $this->url),
             'api' => $this->when(isset($this->api_url_v2), $this->api_url_v2),
         ];
     }
