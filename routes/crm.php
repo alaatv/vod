451f1f83f99d76a4a640a112cbc0b4809f7fbc22
arshia <arshiayazdian@gmail.com>
2023-12-23 15:57:03 +0330

    Debug CRM Api Routes

--- routes/crm.php
+++ routes/crm.php
@@ -27,24 +27,24 @@ Route::prefix('v2')->group(function () {
     Route::resource('ticketPriority', '\\'.TicketPriorityController::class)->only(['index']);
     Route::resource('ticketDepartment', '\\'.TicketDepartmentController::class, ['as' => 'api'])->only(['index']);
     Route::resource('ticketMessage', '\\'.TicketMessageController::class)->except(['create', 'edit']);
-});
 
-// Coupon Routes
-Route::group(['prefix' => 'coupon', 'as' => 'coupon.'], function () {
-    Route::resource('', '\\'.CouponController::class)->except(['create', 'edit']);
-    Route::get('findByCode', [CouponController::class, 'findByCode'])->name('findByCode');
-    Route::post('generateMassiveRandomCoupon',
-        [CouponController::class, 'generateMassiveRandomCoupon'])->name('massive.random');
-});
-Route::post('/savePenaltyCoupon', [CouponController::class, 'savePenaltyCoupon'])->name('save.penalty.coupon');
+    // Coupon Routes
+    Route::group(['prefix' => 'coupon', 'as' => 'coupon.'], function () {
+        Route::resource('', '\\'.CouponController::class)->except(['create', 'edit']);
+        Route::get('findByCode', [CouponController::class, 'findByCode'])->name('findByCode');
+        Route::post('generateMassiveRandomCoupon',
+            [CouponController::class, 'generateMassiveRandomCoupon'])->name('massive.random');
+    });
+    Route::post('/savePenaltyCoupon', [CouponController::class, 'savePenaltyCoupon'])->name('save.penalty.coupon');
 
-// Referral Code routes
-Route::prefix('referral-code')->name('api.v2.referral-code.')->group(function () {
-    Route::get('/', [ReferralCodesController::class, 'index'])->name('index');
-    Route::get('/orderproducts', [ReferralCodesController::class, 'indexOrderproducts'])->name('orderproducts');
-    Route::get('/noneProfitableOrderproducts',
-        [ReferralCodesController::class, 'indexNoneProfitableOrderproducts'])->name('orderproducts.noneProfitable');
-    Route::get('/{referralCode}', [ReferralCodesController::class, 'show'])->name('show');
-    Route::post('/batch-store', [ReferralCodesController::class, 'batchStore'])->name('batch-store');
-    Route::post('/{referralCode}/assign', [ReferralCodesController::class, 'assign'])->name('assign');
+    // Referral Code routes
+    Route::prefix('referral-code')->name('api.v2.referral-code.')->group(function () {
+        Route::get('/', [ReferralCodesController::class, 'index'])->name('index');
+        Route::get('/orderproducts', [ReferralCodesController::class, 'indexOrderproducts'])->name('orderproducts');
+        Route::get('/noneProfitableOrderproducts',
+            [ReferralCodesController::class, 'indexNoneProfitableOrderproducts'])->name('orderproducts.noneProfitable');
+        Route::get('/{referralCode}', [ReferralCodesController::class, 'show'])->name('show');
+        Route::post('/batch-store', [ReferralCodesController::class, 'batchStore'])->name('batch-store');
+        Route::post('/{referralCode}/assign', [ReferralCodesController::class, 'assign'])->name('assign');
+    });
 });
