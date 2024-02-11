a30799cb312871190b1e9e2ffa24c7f77356f52c
sohrab <sohrab.a.fard@gmail.com>
2024-02-11 16:42:25 +0330

    bug fixed and remove un used seeder

--- config/database.php
+++ config/database.php
@@ -167,6 +167,8 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'serializer' => Redis::SERIALIZER_JSON,
+            'compression' => Redis::COMPRESSION_LZ4
         ],
 
         'default' => [
